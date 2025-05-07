5465dc155f1d30616686aa0930fe6bdcd579e71e
Harvar3105 <Petrochenko03@inbox.ru>
2025-05-07 12:48:52 +0300

    Added pinia and eslint

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router";
 import i18n from "./i18n/i18n";
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(i18n);
+app.use(createPinia());
 
 app.mount('#app');
