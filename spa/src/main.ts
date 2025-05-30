0fcd626f8a253f95db8d902a32fda03f319527da
Harvar3105 <Petrochenko03@inbox.ru>
2025-05-30 10:59:42 +0300

    added error handler

--- spa/src/main.ts
+++ spa/src/main.ts
@@ -1,15 +1,20 @@
 import './assets/tailwind.css'
 
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import router from "./router";
 import i18n from "./i18n/i18n";
 import { createPinia } from 'pinia';
 import App from "./App.vue";
 import {useUserStore} from "@/store/UserStore.ts";
+import {handleError} from "@/utils/errorHandler.ts";
 
 const app = createApp(App);
 app.use(createPinia());
 app.use(i18n);
+
+app.config.errorHandler = (err, instance, info) => {
+    handleError(err, `Vue error (${info})`);
+};
 
 (async () => {
     await useUserStore().init();
---
+++ spa/src/utils/errorHandler.ts
@@ -0,0 +1,17 @@
+﻿export function handleError(error: unknown, context?: string): void {
+    const errorMessage = extractErrorMessage(error);
+
+    console.error(`[${context || 'App'} Error]:`, errorMessage);
+
+    alert(`Error occurred: ${context ? ' in ' + context : ''}: ${errorMessage}`);
+}
+
+function extractErrorMessage(error: unknown): string {
+    if (error instanceof Error) {
+        return error.message;
+    }
+    if (typeof error === 'string') {
+        return error;
+    }
+    return 'Unknown error occurred.';
+}
